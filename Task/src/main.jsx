6db2b791d65f5966eacd2205f87d13c3ef5d6a97
Bhavani-gudumala <bhavanigudumala@gmail.com>
2024-11-22 13:16:13 +0530

    test comitted.

--- Task/src/main.jsx
+++ Task/src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import seed from './files/seed.jsx'
+import pool from './files/pool.jsx'
+import fetchOrders from './files/fetchOrders.jsx'
+import VirtualizedOrdersTable from './files/VirtualizedOrdersTable .jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App /> 
+    <seed/>
+    <pool/>
+    <fetchOrders/>
+    <VirtualizedOrdersTable/> 
   </StrictMode>,
 )
